f947c5afef323f39c91335ba7937129338c76e9f
Ihor <dark.man.i.1987@gmail.com>
2024-10-09 20:10:49 +0300

    Removed windwos crtdbg

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define _CRTDBG_MAP_ALLOC
 #include "stdio.h"
 #include "stdlib.h"
 #include <thread>
@@ -14,106 +12,8 @@
 #include "image-service/image-service.hpp"
 #include "mongo-client/mongo-client.hpp"
 
-// #define STB_IMAGE_IMPLEMENTATION
-// #define STBI_NO_PSD
-// #define STBI_NO_TGA
-// #define STBI_NO_GIF
-// #define STBI_NO_HDR
-// #define STBI_NO_PIC
-//// #define STBI_NO_STDIO
-// #include "stb-image/stb_image.h"
-// #ifndef STB_IMAGE_WRITE_IMPLEMENTATION
-// #define STB_IMAGE_WRITE_IMPLEMENTATION
-// #include "stb-image/stb_image_write.h"
-// #endif
-
-#include <crtdbg.h>
-
 int main(int argc, char **argv) {
-  // ncnn::create_gpu_instance();
-  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-  // Send all reports to STDOUT
-  _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
-  _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDOUT);
-  _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDOUT);
-
-  _CrtMemState s1, s2, s3;
-  _CrtMemCheckpoint(&s1);
-
-  // int x, y, c;
-  // std::string ext = ".png";
-  // std::string fullinput = "";
-  // fullinput += "input";
-  // unsigned char *pixeldataOne = stbi_load("cat.jpg", &x, &y, &c, 0);
-  //
-  // int x1, y1, c1;
-  // unsigned char *pixeldataTwo = stbi_load((fullinput + "two" + ext).c_str(), &x1, &y1, &c1, 0);
-  //
-  //   ncnn::Net net;
-  // int vdevice = ncnn::get_default_gpu_index();
-  //   net.opt.use_vulkan_compute = true;
-  // net.opt.use_fp16_packed = false;    // TODO:true
-  // net.opt.use_fp16_storage = false;   // TODO:true
-  // net.opt.use_fp16_arithmetic = false;
-  // net.opt.use_int8_storage = false;   // TODO:true
-  // net.opt.use_int8_arithmetic = false;
-  // net.set_vulkan_device(vdevice);
-  //  int erz = 0;
-  //{
-  //   FILE *fp = fopen("../models/realesrgan-x4plus.param", "rb");
-  //   if (!fp) {
-  //     return -1;
-  //   }
-  //   erz = net.load_param(fp);
-  //   fclose(fp);
-  // }
-  //{
-  //   FILE *fp = fopen("../models/realesrgan-x4plus.bin", "rb");
-  //   if (!fp) {
-  //     return -1;
-  //   }
-  //   erz |= net.load_model(fp);
-  //   fclose(fp);
-  // }
-  // if (erz != 0) {
-  //   return -1;
-  // }
-
-  // limb::RealesrganService ns(&net, false);
-  // auto ret = ns.load();
-  // if (ret != liret::kOk) {
-  //   printf("%s", listat::getErrorMessage(ret));
-  //   return (int)ret;
-  // }
-
-  // ncnn::Mat inimageOne(x, y, (void *)pixeldataOne, (size_t)c, c);
-  // ncnn::Mat outimageOne(x * 4, y * 4, (size_t)c, c);
-
-  //  ncnn::Mat inimageTwo(x1, y1, (void *)pixeldataTwo, (size_t)c1, c1);
-  // ncnn::Mat outimageTwo(x1 * 4, y1 * 4, (size_t)c1, c1);
-  // ns.scale = 4;
-  // ns.tilesize = 200;
-  // ns.prepadding = 10;
-
-  // auto executionLambda = [](limb::RealesrganService*ns,ncnn::Mat *inimage, ncnn::Mat *outimage) {
-  //   //ns->process_matrix(*inimage, *outimage);
-  // };
-  // std::thread thx(executionLambda,&ns,&inimageOne,&outimageOne);
-  //
-  // std::thread thx1(executionLambda, &ns, &inimageTwo, &outimageTwo);
-  // thx.join();
-  // thx1.join();
-  // stbi_write_png((fullinput + "one" + "out" + ext).c_str(), x * 4, y * 4, c, outimageOne.data, 0);
-  // stbi_write_png((fullinput + "two" + "out" + ext).c_str(), x1 * 4, y1 * 4, c1, outimageTwo.data, 0);
-  // delete pixeldataOne;
-  // delete pixeldataTwo;
-
-  //  ^^^^^^^^^^^^^^^
-  // Initialise MongoService
   mongoc_init();
   ncnn::create_gpu_instance();
 
@@ -162,6 +62,8 @@ int main(int argc, char **argv) {
   }
   std::cout << " [x] Awaiting RPC requests" << std::endl;
 
+  // For some reason windwos SIGTERM produces thousands of memory leak
+  // Seems like on windows it better aproach
   // std::thread thd([&handler]() {
   //   Sleep(3000);
   //   handler.quit();
@@ -178,14 +80,6 @@ int main(int argc, char **argv) {
 
   delete limb::imageService;
   ncnn::destroy_gpu_instance();
-
-  _CrtMemCheckpoint(&s2);
-  if (_CrtMemDifference(&s3, &s1, &s2)) {
-    std::cout << "Memory leaks detected in the specific code region!\n";
-    _CrtMemDumpStatistics(&s3);
-  } else {
-    std::cout << "No memory leaks detected in the specific code region.\n";
-  }
 
   return 0;
 }
