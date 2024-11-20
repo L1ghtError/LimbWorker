0dcbb8092e6937941dc1a5ce5061785c752e8816
L1ghtError <dark.man.i.1987@gmail.com>
2024-11-20 20:34:15 +0200

    Started to implement tests

---
+++ tests/thread_pool.t.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+
+#include <thread-pool/thread-pool.hpp>
+
+#include <thread>
+#include <future>
+#include <functional>
+#include <memory>
+
+namespace TestLinkage {
+size_t getWorkerIdForCurrentThread()
+{
+    return *limb::tp::detail::thread_id();
+}
+
+size_t getWorkerIdForCurrentThread2()
+{
+    return limb::tp::Worker<std::function<void()>, limb::tp::MPMCBoundedQueue>::getWorkerIdForCurrentThread();
+}
+}
+
+TEST(ThreadPool, postJob)
+{
+    limb::tp::ThreadPool pool;
+
+    std::packaged_task<int()> t([]()
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            return 42;
+        });
+
+    std::future<int> r = t.get_future();
+
+    pool.post(t);
+
+    ASSERT_EQ(42, r.get());
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
