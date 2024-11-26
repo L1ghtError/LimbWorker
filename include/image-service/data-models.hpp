3310efb2e173b2e69ff6533f8b498a4c25c039d0
L1ghtError <dark.man.i.1987@gmial.com>
2024-11-26 09:27:09 +0200

    reimplementation of the amqp driver, and small fixes

--- include/image-service/data-models.hpp
+++ include/image-service/data-models.hpp
@@ -1,17 +1,12 @@
 #ifndef _DATA_MODELS_HPP_
 #define _DATA_MODELS_HPP_
-#include "utils/endian.h"
 #include "utils/status.h"
 #include <cstdint>
 
 namespace limb {
 
 struct MUpscaleImage {
-  inline liret berawtoh(const char *raw) {
-    *this = *(limb::MUpscaleImage *)(raw);
-    modelId = be32toh(modelId);
-    return liret::kOk;
-  }
+  liret berawtoh(const char *raw);
   uint32_t modelId;
   uint8_t imageId[24];
 };
